b1a84eb1b702d2e83643bac38a0861b408a21aff
David <71690194+davidiheanacho@users.noreply.github.com>
2021-03-03 08:29:43 +0200

    Add files via upload

---
+++ abba.php
@@ -0,0 +1,26 @@
+<?php
+function homeWork($string,$number)
+{
+    $len = strlen($string);
+    $count = 0;
+
+
+    $sameletter = $string[0];
+    for ($i = 0; $i < $len; $i++) {
+        if ($string[$i] == $sameletter) {
+            $count++;
+            if ($count > $number) {
+                $result = str_split('');
+               // array_splice($result, $i, 1);
+                //implode('', $result);
+                //$i--;
+            }
+        } else {
+            $sameletter = $string[$i];
+            $count = 1;
+        }
+    }
+    echo $string;
+}
+
+homeWork('aaab', 2);
